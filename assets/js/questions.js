13c77a6fd25054bb664c82dce7c192a95bc4347e
Obee123 <aobiaderi@yahoo.com>
2023-01-16 15:49:37 +0000

    added questions to question js file

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+// Questions array and object  
+var questions = [
+    {
+        title: "What year did the battle of Hastings take place?",
+        choices: ["1888", "1066", "1415", "1106"],
+        answer: "1066"
+    },
+    {
+        title: "How many wives did Henry VIII have?",
+        choices: ["six", "one", "five", "seven"],
+        answer: "six"
+    },
+    {
+        title: "What was the nickname given to Margaret Thatcher?",
+        choices: ["Coal Lady", "Iron Lady", "Queen Mags", "all of the above"],
+        answer: "Iron Lady"
+    },
+    {
+        title: "Which party was Gordon Brown the leader of?",
+        choices: ["Labour Party", "Lib Dems", "Conservatives", "Green Party"],
+        answer: "Labour Party"
+    },
+    {
+        title: "Who founded the Church of England?",
+        choices: ["Richard I", "Henry VIII", "Richard III", "Edward The Martyr"],
+        answer: "Henry VIII"
+    },
+
+];
