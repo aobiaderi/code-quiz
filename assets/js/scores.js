c42e69ea9cdf3e8ed3799acf98b15f2f083bf172
Obee123 <aobiaderi@yahoo.com>
2023-01-20 21:29:40 +0000

    retrieved highscores from local storage and looped through the array

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,15 @@
 const highscoreContainer = document.getElementById("highscores")
 const clearscoreBtn = document.getElementById("clear")
 
+// Get the highscores from local storage
+const highScores = JSON.parse(localStorage.getItem("highScore"))
+
+// Loop through the highscores array
+ for (let i = 0; i < highScores.length; i++){
+     // Create a new LI element
+    let liEl = document.createElement("li")
+     // Set the text of the LI element to the current highscore and user initials
+    liEl.textContent = highScores[i].initials + "|" + highScores[i].userScore;
+    // Append the LI element to the OL element
+    highscoreContainer.append(liEl)
+ }
