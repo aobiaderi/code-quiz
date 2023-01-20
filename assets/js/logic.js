8a7e5bb866062dcee200146e8667e32862ff76d6
Obee123 <aobiaderi@yahoo.com>
2023-01-20 20:33:27 +0000

    created endQuiz function and set time interval for timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,8 +7,6 @@ let timer = 75;
 let incorrectSound = new Audio("./assets/sfx/incorrect.wav");
 let correctSound = new Audio("./assets/sfx/correct.wav");
 
-
-
 function startQuiz() {
 // Start timer
 // hide the instructions befoe user clicks to start quiz
@@ -36,4 +34,21 @@ document.getElementById("start-screen").style.display = "none";
 
 };
 
+function endQuiz() {
+    score =timer +1;
+
+  // declare variable to store the value of the time at the end of the quiz
+  let finalTime = document.getElementById("time").innerHTML;
+
+  // // Stop the timer by passing the setInterval variable to the clearInterval function.
+  clearInterval(timeCountdown); 
+  document.getElementById("end-screen").style.display = "block";
+  document.getElementById("time").innerHTML = finalTime + " seconds"; // Display the final time
+
+  document.getElementById("final-score").innerHTML = score + " points"; // Display the final score
+
+  // hide the questions at the end of the quiz
+  document.getElementById("questions").style.display = "none";
+  
+}
 
