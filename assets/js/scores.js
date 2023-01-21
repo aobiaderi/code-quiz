9e2008cd010c37c50241ec041fe2e79b1265ec2b
Obee123 <aobiaderi@yahoo.com>
2023-01-21 10:02:08 +0000

    added event listener to clear highscore

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -13,3 +13,13 @@ const highScores = JSON.parse(localStorage.getItem("highScore"))
     // Append the LI element to the OL element
     highscoreContainer.append(liEl)
  }
+
+ // Add eventlistener to the clear highscore button to remove highscores from both the local storage and the display
+clearscoreBtn.addEventListener("click",function(){
+    // remove the highScore from the local storage
+    localStorage.removeItem("highScore")
+    // remove all of the <li> elements from the <ol> element using a while loop
+    while (highscoreContainer.firstChild) {
+highscoreContainer.removeChild(highscoreContainer.firstChild);
+    }
+});
